90860246c8c804fa3f4de45491d1b96a932ffafa
g <you@example.com>
2023-05-20 17:39:25 +0200

    working hello wrold

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use obws::Client;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Connect to the OBS instance through obs-websocket.
+    let client = Client::connect("localhost", 4455, Some("secret")).await?;
+
+    // Get and print out version information of OBS and obs-websocket.
+    let version = client.general().version().await?;
+    println!("{version:#?}");
+
+    // Get a list of available scenes and print them out.
+    let scene_list = client.scenes().list().await?;
+    println!("{scene_list:#?}");
+
+    Ok(())
 }
